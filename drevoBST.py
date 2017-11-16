193cb8790a92d4c4ca28687340cb492175489d07
martincesnovar <martin.cesnovar@gmail.com>
2017-11-16 17:59:19 +0100

    Create drevoBST.py

---
+++ drevoBST.py
@@ -0,0 +1,78 @@
+from dvojisko_drevo import Drevo
+
+mini=float('-inf')
+maxi=float('inf')
+
+def je_iskalno(drevo):
+    ''' Vrne ali je dvojiško drevo iskalno.'''
+    return je_iskalno_drevo_rek(drevo, mini=mini, maxi=maxi)
+
+
+def je_iskalno_drevo_rek(drevo, mini, maxi):
+    '''mini < drevo.podatek < maxi'''
+    if drevo.prazno:
+        return True
+    
+    if drevo.podatek < mini or drevo.podatek > maxi:
+        return False
+
+    return (je_iskalno_drevo_rek(drevo.levo, mini, drevo.podatek-1) and
+          je_iskalno_drevo_rek(drevo.desno, drevo.podatek+1, maxi))
+
+def vmesni_pregled(drevo):
+    '''vmesni pregled dreves'''
+    if not drevo.prazno:
+        for levi in vmesni_pregled(drevo.levo):
+            yield levi   
+        yield drevo.podatek
+        for desni in vmesni_pregled(drevo.desno):
+            yield desni
+
+def je_iskalno_vmesni(drevo):
+    '''s pomočjo vmesnega pregleda preveri ali je drevo iskalno'''
+    prejsni = float('-inf')
+    for podatek in vmesni_pregled(drevo):
+        if prejsni >= podatek:
+            return False
+        prejsni = podatek
+    return True #prišli smo do konca
+
+
+if __name__ == '__main__':
+    prazno = Drevo()
+    en_el = Drevo(0)
+    d = Drevo(5,
+              levo=Drevo(3),
+              desno=Drevo(6))
+    
+    ni_iskalno = Drevo(2,
+                       levo=Drevo(3),
+                       desno=Drevo(6))
+    
+    drevo_1 = Drevo(25,
+                  levo=Drevo(13,
+                             levo=Drevo(11,
+                                        levo = Drevo(-4),desno=Drevo(12)),
+                             desno = Drevo(17,
+                                           levo=Drevo(15),
+                                           desno = Drevo(20))),
+                  desno=Drevo(42,
+                              levo=Drevo(30,
+                                         levo = Drevo(27),
+                                         desno = Drevo(40)),
+                              desno=Drevo(45)))
+
+
+
+    
+    print(je_iskalno(prazno))
+    print(je_iskalno(d))
+    print(je_iskalno(en_el))
+    print(je_iskalno(ni_iskalno))
+    print(je_iskalno(drevo_1))
+    print(je_iskalno_vmesni(prazno))
+    print(je_iskalno_vmesni(en_el))
+    print(je_iskalno_vmesni(ni_iskalno))
+    print(je_iskalno_vmesni(drevo_1))
+
+    
