caa7c49d3c3349b984a125da697baa2749e28e88
Martin Češnovar <martin.cesnovar@gmail.com>
2017-11-19 20:02:18 +0100

    drevoBST.py

--- drevoBST.py
+++ drevoBST.py
@@ -1,11 +1,8 @@
 from dvojisko_drevo import Drevo
 
-mini=float('-inf')
-maxi=float('inf')
-
 def je_iskalno(drevo):
     ''' Vrne ali je dvojiško drevo iskalno.'''
-    return je_iskalno_drevo_rek(drevo, mini=mini, maxi=maxi)
+    return je_iskalno_drevo_rek(drevo, mini=float('-inf'), maxi=float('inf'))
 
 
 def je_iskalno_drevo_rek(drevo, mini, maxi):
@@ -19,6 +16,7 @@ def je_iskalno_drevo_rek(drevo, mini, maxi):
     return (je_iskalno_drevo_rek(drevo.levo, mini, drevo.podatek-1) and
           je_iskalno_drevo_rek(drevo.desno, drevo.podatek+1, maxi))
 
+
 def vmesni_pregled(drevo):
     '''vmesni pregled dreves'''
     if not drevo.prazno:
@@ -28,8 +26,9 @@ def vmesni_pregled(drevo):
         for desni in vmesni_pregled(drevo.desno):
             yield desni
 
+
 def je_iskalno_vmesni(drevo):
-    '''s pomočjo vmesnega pregleda preveri ali je drevo iskalno'''
+    '''z vmesnim pregledom preveri ali je drevo iskalno'''
     prejsni = float('-inf')
     for podatek in vmesni_pregled(drevo):
         if prejsni >= podatek:
@@ -39,6 +38,7 @@ def je_iskalno_vmesni(drevo):
 
 
 if __name__ == '__main__':
+    drevo_2 = Drevo(2,levo=Drevo(2),desno=Drevo(3))
     prazno = Drevo()
     en_el = Drevo(0)
     d = Drevo(5,
@@ -61,18 +61,16 @@ if __name__ == '__main__':
                                          levo = Drevo(27),
                                          desno = Drevo(40)),
                               desno=Drevo(45)))
-
-
-
     
     print(je_iskalno(prazno))
     print(je_iskalno(d))
+    print(je_iskalno(drevo_2))
     print(je_iskalno(en_el))
     print(je_iskalno(ni_iskalno))
     print(je_iskalno(drevo_1))
+    print('----')
     print(je_iskalno_vmesni(prazno))
+    print(je_iskalno_vmesni(drevo_2))
     print(je_iskalno_vmesni(en_el))
     print(je_iskalno_vmesni(ni_iskalno))
     print(je_iskalno_vmesni(drevo_1))
-
-    
